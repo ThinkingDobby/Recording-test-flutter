188152f2080774cde2f53ddc871ed3ee3f1c85c3
ThinkingDobby <chiho1221@naver.com>
2022-07-21 17:21:02 +0900

    녹음 기초 구현

--- lib/recorder.dart
+++ lib/recorder.dart
@@ -1,6 +1,36 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
 
-class Recorder extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:intl/intl.dart' show DateFormat;
+import 'package:intl/date_symbol_data_local.dart';
+
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
+
+class Recorder extends StatefulWidget {
+  @override
+  State createState() => _RecorderState();
+}
+
+class _RecorderState extends State<Recorder> {
+  late FlutterSoundRecorder _recordingSession;
+  final recordingPlayer = AssetsAudioPlayer();
+
+  bool _isPlaying = false;
+
+  late String filePath;
+  String _timerText = '0';
+
+
+  @override
+  void initState() {
+    super.initState();
+    initializer();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,15 +41,84 @@ class Recorder extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              ElevatedButton(onPressed: null, child: Text("녹음 시작")),
+              ElevatedButton(onPressed: startRecording, child: Text("녹음 시작")),
               const SizedBox(width:16),
-              ElevatedButton(onPressed: null, child: Text("녹음 중지"))
+              ElevatedButton(onPressed: stopRecording, child: Text("녹음 중지")),
             ],
           ),
           const SizedBox(height: 22),
-
+          Text(_timerText),
+          const SizedBox(height: 22),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _isPlaying = !_isPlaying;
+                });
+                if (_isPlaying) {
+                  startPlaying();
+                } else {
+                  stopPlaying();
+                }
+              },
+              child: Text(_isPlaying ? "중지" : "재생")),
         ],
       ),
     );
+  }
+
+  void initializer() async {
+    filePath = '/sdcard/Download/temp.wav';
+    _recordingSession = FlutterSoundRecorder();
+    await _recordingSession.openAudioSession(
+        focus: AudioFocus.requestFocusAndStopOthers,
+        category: SessionCategory.playAndRecord,
+        mode: SessionMode.modeDefault,
+        device: AudioDevice.speaker);
+    await _recordingSession.setSubscriptionDuration(const Duration(
+        milliseconds: 10));
+    await initializeDateFormatting();
+    await Permission.microphone.request();
+    await Permission.storage.request();
+    await Permission.manageExternalStorage.request();
+  }
+
+  Future<void> startRecording() async {
+    Directory directory = Directory(dirname(filePath));
+    if (!directory.existsSync()) {
+      directory.createSync();
+    }
+    _recordingSession.openAudioSession();
+    await _recordingSession.startRecorder(
+      toFile: filePath,
+      codec: Codec.pcm16WAV,
+    );
+    StreamSubscription recorderSubscription =
+    _recordingSession.onProgress!.listen((e) {
+      var date = DateTime.fromMillisecondsSinceEpoch(
+          e.duration.inMilliseconds,
+          isUtc: true);
+      var timeText = DateFormat('mm:ss:SS', 'en_GB').format(date);
+      setState(() {
+        _timerText = timeText.substring(0, 8);
+      });
+    });
+    recorderSubscription.cancel();
+  }
+
+  Future<String?> stopRecording() async {
+    _recordingSession.closeAudioSession();
+    return await _recordingSession.stopRecorder();
+  }
+
+  Future<void> startPlaying() async {
+    recordingPlayer.open(
+      Audio.file(filePath),
+      autoStart: true,
+      showNotification: true,
+    );
+  }
+
+  Future<void> stopPlaying() async {
+    recordingPlayer.stop();
   }
 }
