c41ee17c596052f2ee09e49d677b392fafbb426e
ThinkingDobby <chiho1221@naver.com>
2022-07-22 22:00:58 +0900

    리스트뷰 구현 중 임시저장

--- lib/recorder.dart
+++ lib/recorder.dart
@@ -21,8 +21,11 @@ class _RecorderState extends State<Recorder> {
   bool _isRecording = false;
   bool _isPlaying = false;
 
-  late String filePath;
+  late String _storagePath;
+  late String _filePathForRecord;
+  late String _selectedFile;
 
+  List<String> _fileList = <String>[];
 
   @override
   void initState() {
@@ -41,56 +44,136 @@ class _RecorderState extends State<Recorder> {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              ElevatedButton(onPressed: _isRecording ? null : startRecording, child: Text("녹음 시작")),
-              const SizedBox(width:16),
-              ElevatedButton(onPressed: _isRecording ? stopRecording : null, child: Text("녹음 중지")),
+              ElevatedButton(
+                  onPressed: _isRecording ? null : startRecording,
+                  child: Text("녹음 시작")),
+              const SizedBox(width: 16),
+              ElevatedButton(
+                  onPressed: () {
+                    _isRecording ? stopRecording() : null;
+                    setState(() {
+                      _fileList = loadFiles();
+                    });
+                  },
+                  child: Text("녹음 중지")),
             ],
           ),
           const SizedBox(height: 22),
           ElevatedButton(
               onPressed: () {
-                setState(() {
-                  _isPlaying = !_isPlaying;
-                });
-                if (_isPlaying) {
-                  startPlaying();
-                } else {
-                  stopPlaying();
+                if (_fileList.isNotEmpty) {
+                  setState(() {
+                    _isPlaying = !_isPlaying;
+                  });
+                  if (_isPlaying) {
+                    startPlaying();
+                  } else {
+                    stopPlaying();
+                  }
                 }
               },
               child: Text(_isPlaying ? "중지" : "재생")),
+          const SizedBox(height: 32),
+          Flexible(
+              flex: 1,
+              fit: FlexFit.tight,
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: _fileList.length,
+                itemBuilder: (context, i) => _setListItemBuilder(context, i),
+              )),
+          Container(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 32),
+              child: ElevatedButton(
+                  onPressed: () {
+                    deleteFiles();
+                    setState(() {
+                      _fileList = loadFiles();
+                    });
+                  },
+                  child: Text("전체 삭제")))
         ],
       ),
     );
   }
 
   void initializer() async {
-    filePath = '/sdcard/Download/temp.wav';
+    _storagePath = '/sdcard/Download';
+    setState(() {
+      _fileList = loadFiles();
+    });
+
     _recordingSession = FlutterSoundRecorder();
     await _recordingSession.openAudioSession(
         focus: AudioFocus.requestFocusAndStopOthers,
         category: SessionCategory.playAndRecord,
         mode: SessionMode.modeDefault,
         device: AudioDevice.speaker);
-    await _recordingSession.setSubscriptionDuration(const Duration(
-        milliseconds: 10));
+    await _recordingSession
+        .setSubscriptionDuration(const Duration(milliseconds: 10));
     await initializeDateFormatting();
     await Permission.microphone.request();
     await Permission.storage.request();
     await Permission.manageExternalStorage.request();
   }
 
+  List<String> loadFiles() {
+    List<String> files = <String>[];
+
+    var dir = Directory(_storagePath).listSync();
+    for (var file in dir) {
+      if (checkExtWav(file.path)) {
+        files.add(getFilenameFromPath(file.path));
+      }
+    }
+
+    if (files.isNotEmpty) {
+      _selectedFile = files[0];
+    }
+
+    files.sort();
+    _filePathForRecord = '$_storagePath/temp${files.length + 1}.wav';
+
+    return files;
+  }
+
+  bool checkExtWav(String fileName) {
+    if (fileName.substring(fileName.length - 3) == "wav") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  String getFilenameFromPath(String filePath) {
+    int idx = filePath.lastIndexOf('/') + 1;
+    return filePath.substring(idx);
+  }
+
+  RadioListTile _setListItemBuilder(BuildContext context, int i) {
+    return RadioListTile(
+        title: Text(_fileList[i]),
+        value: _fileList[i],
+        groupValue: _selectedFile,
+        onChanged: (val) {
+          setState(() {
+            _selectedFile = _fileList[i];
+          });
+        });
+  }
+
   Future<void> startRecording() async {
     setState(() {
       _isRecording = true;
     });
-    Directory directory = Directory(dirname(filePath));
+    Directory directory = Directory(dirname(_filePathForRecord));
     if (!directory.existsSync()) {
       directory.createSync();
     }
     _recordingSession.openAudioSession();
     await _recordingSession.startRecorder(
-      toFile: filePath,
+      toFile: _filePathForRecord,
       codec: Codec.pcm16WAV,
     );
   }
@@ -105,7 +188,7 @@ class _RecorderState extends State<Recorder> {
 
   Future<void> startPlaying() async {
     recordingPlayer.open(
-      Audio.file(filePath),
+      Audio.file('$_storagePath/$_selectedFile.wav'),
       autoStart: true,
       showNotification: true,
     );
@@ -118,5 +201,14 @@ class _RecorderState extends State<Recorder> {
 
   Future<void> stopPlaying() async {
     recordingPlayer.stop();
+  }
+
+  deleteFiles() {
+    var dir = Directory(_storagePath).listSync();
+    for (var file in dir) {
+      if (checkExtWav(file.path)) {
+        file.delete();
+      }
+    }
   }
 }
